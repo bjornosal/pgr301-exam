23df4173c5a87d4aad4e099e62359d310e22fbe7
Bjørn Olav Salvesen <bjorn.olav.salvesen@gmail.com>
2019-10-19 22:41:30 +0200

    Changed to use own credentials.

--- docker_push.sh
+++ docker_push.sh
@@ -2,5 +2,6 @@
 git ls-files -o
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker build . --tag geiger-api --build-arg JAR_FILE=./target/GeigerApi.jar
-docker tag geiger-api "$DOCKER_USERNAME/geiger-api:latest"
-docker push "$DOCKER_USERNAME/geiger-api"
+#TODO: Add as values in a 'run-first'-script
+docker tag geiger-api bjornosal/geiger-api:latest
+docker push bjornosal/geiger-api
