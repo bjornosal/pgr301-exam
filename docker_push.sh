05377ec1ab11918e8e8b99a11b9b94295911d336
Bjørn Olav Salvesen <bjorn.olav.salvesen@gmail.com>
2019-10-19 17:56:52 +0200

    Altered docker-script

--- docker_push.sh
+++ docker_push.sh
@@ -2,5 +2,5 @@
 git ls-files -o
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker build . --tag geiger-api --build-arg JAR_FILE=./target/GeigerApi.jar
-docker tag geiger-api "$DOCKER_USERNAME"/geiger-api:latest
-docker push "$DOCKER_USERNAME"/geiger-api
+docker tag geiger-api "$DOCKER_USERNAME/geiger-api:latest"
+docker push "$DOCKER_USERNAME/geiger-api"
