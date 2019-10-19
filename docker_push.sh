b827ea7546907e21add3d7ea34e25a12a92dce0b
Bjørn Olav Salvesen <bjorn.olav.salvesen@gmail.com>
2019-10-19 22:46:01 +0200

    Changed back to use variables in script.

--- docker_push.sh
+++ docker_push.sh
@@ -2,6 +2,5 @@
 git ls-files -o
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker build . --tag geiger-api --build-arg JAR_FILE=./target/GeigerApi.jar
-#TODO: Add as values in a 'run-first'-script
-docker tag geiger-api bjornosal/geiger-api:latest
-docker push bjornosal/geiger-api
+docker tag geiger-api "$DOCKER_USERNAME/geiger-api:latest"
+docker push "$DOCKER_USERNAM E/geiger-api"
