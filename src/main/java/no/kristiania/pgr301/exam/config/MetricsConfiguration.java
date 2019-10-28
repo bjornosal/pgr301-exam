0878ca374362ef44debf4ff2635506e5fa449c5f
Bjørn Olav Salvesen <bjorn.olav.salvesen@gmail.com>
2019-10-28 17:39:29 +0100

    Renamed logback file. Excluded influxdbautoconfiguration in production

--- src/main/java/no/kristiania/pgr301/exam/config/MetricsConfiguration.java
+++ src/main/java/no/kristiania/pgr301/exam/config/MetricsConfiguration.java
@@ -1,29 +1,19 @@
 package no.kristiania.pgr301.exam.config;
 
-import io.micrometer.core.instrument.Clock;
 import io.micrometer.core.instrument.MeterRegistry;
 import io.micrometer.core.instrument.simple.SimpleMeterRegistry;
-import io.micrometer.influx.InfluxConfig;
-import io.micrometer.influx.InfluxMeterRegistry;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 @Configuration
+@Slf4j
+@Profile("prod")
 public class MetricsConfiguration {
 
   @Bean
-  @Profile("prod")
   public MeterRegistry productionMeterRegistry() {
-    // TODO: 20.10.2019
-    //  Sett noe standard config på denne?
     return new SimpleMeterRegistry();
-  }
-
-  @Bean
-  @ConditionalOnMissingBean
-  public InfluxMeterRegistry nonProductionMeterRegistry(InfluxConfig config, Clock clock) {
-    return new InfluxMeterRegistry(config, clock);
   }
 }
